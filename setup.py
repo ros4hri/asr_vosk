1f1a6215e20c22f43a44bc88e2233cb81fed7779
Luka Juricic <luka.juricic@pal-robotics.com>
2024-07-18 17:51:20 +0200

    2.2.1

--- setup.py
+++ setup.py
@@ -25,7 +25,7 @@ except ImportError:
 
 setup(
     name=package_name,
-    version='2.2.0',
+    version='2.2.1',
     packages=[package_name],
     data_files=[
         ('share/ament_index/resource_index/packages', ['resource/' + package_name]),
